70f6cd50cca15865b6388a7b7e36f007f87429f9
andfxx27 <idea.andersen@gmail.com>
2023-07-04 23:07:10 +0700

    feat: p7.2 - algorithm; selection sort part 3

--- src/main/java/andfxx/p7/algorithms/Algorithms.java
+++ src/main/java/andfxx/p7/algorithms/Algorithms.java
@@ -1,5 +1,7 @@
 package andfxx.p7.algorithms;
 
+import java.util.Arrays;
+
 public class Algorithms {
     public static void algorithms() {
         System.out.println("Hello world from andfxx.p7.algorithms");
@@ -8,9 +10,29 @@ public class Algorithms {
     }
 
     private static void sorting() {
-        int numbers[] = {6, 5, 8, 7, 11};
+        int[] numbers = {6, 5, 8, 7, 11};
 
-        System.out.println(smallest(numbers));
+        System.out.println(indexOfSmallestFrom(numbers, 0));
+        System.out.println(indexOfSmallestFrom(numbers, 1));
+        System.out.println(indexOfSmallestFrom(numbers, 2));
+    }
+
+    private static int indexOfSmallestFrom(int[] table, int startIndex) {
+        return -1;
+    }
+
+    private static int indexOfSmallest(int[] array) {
+        int smallestValue = smallest(array);
+
+        int idx = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == smallestValue) {
+                return i;
+            }
+        }
+
+        return idx;
     }
 
     private static int smallest(int[] array) {
